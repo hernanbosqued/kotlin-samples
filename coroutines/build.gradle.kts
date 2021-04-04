002ee18fdda050c625dc47fe7e077f21d1360826
hernanbosqued <hernanbosqued@gmail.com>
2021-04-04 09:23:51 -0300

    changed hello

--- coroutines/build.gradle.kts
+++ coroutines/build.gradle.kts
@@ -1,4 +1,22 @@
+plugins {
+    application
+}
+
+application {
+    mainClassName = "hernanbosqued.samples.coroutines.CoroutinesKt"
+}
+
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
     implementation("junit:junit:4.12")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = application.mainClassName
+    }
+
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
 }
